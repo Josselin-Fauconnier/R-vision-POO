c2165890f5d979a9af81cbbd06b90e7d96a970de
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-10-03 16:40:45 +0200

    début

---
+++ Jour1.php
@@ -0,0 +1,22 @@
+<?php
+
+class Product {
+    
+    private int $id;
+    private string $name;
+    private array $photos;
+    private int $prive;
+    private string $description;
+    private int $quantity;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+
+    private function getId() : int {
+        
+    }
+
+}
+
+
+?>
